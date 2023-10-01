e414658dd5b1d627fbda6fece3a54cc6642a696d
kwaadrat <50247454+kwaadrat@users.noreply.github.com>
2023-10-01 20:08:17 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+window_size =(500, 500)
+
+screen = pygame.display.set_mode(window_size)
+
+background = (230, 230, 230)
+
+running = True
+while running :
+    for event in pygame.event.ge():
+        if event.type == pygame.QUIT:
+            running = False
+
+            screen.fill(background)
+
+    pygame.draw.rect(screen, (100, 150, 100), (100, 100, 100, 300, 300))
