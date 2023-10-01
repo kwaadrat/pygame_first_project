476f5df3e9144a0fc989b5b467c030095f3a910d
kwaadrat <50247454+kwaadrat@users.noreply.github.com>
2023-10-01 20:10:35 +0200

    new

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ background = (230, 230, 230)
 
 running = True
 while running :
-    for event in pygame.event.ge():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
             screen.fill(background)
 
     pygame.draw.rect(screen, (100, 150, 100), (100, 100, 100, 300, 300))
+
+    pygame.display.flip()
+
+pygame.quit()
